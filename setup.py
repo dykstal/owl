cc6729b0339b2997766fd6f2c696a2a8432004eb
Aidan Dykstal <dykstala@gmail.com>
2020-08-09 12:08:04 -0600

    Bump Version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def getRequirements():
 # Initalize Package Setup Upon Pip Installation
 setup(
     name = 'owl',
-    version = '0.1.0',
+    version = '0.1.1',
     python_requires = '>=3.6',
     description = DESC,
     long_description = DESC,
